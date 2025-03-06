41c99efc2e475e0a68983ca873e3ad3a9254a53d
bindushree-b <151712594+bindushree-b@users.noreply.github.com>
2025-03-06 07:28:22 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 
+    + 3) // code readability should be high
+
+console.log("Bindu")
+
+let name = "Bindu"
+let age = 20
+let isLoggedIn = false
+
+// primitive data types
+// number => 2^53
+// bigint => big number
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => value not assigned
+// symbol => to find uniqueness
+
+//object
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
